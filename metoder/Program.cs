1836fdd6a4279e82f2fd352483934f7ade18d5f8
Hugge25 <hibba04@hotmail.se>
2023-02-10 10:24:42 +0100

    Add project files.

---
+++ metoder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System; 
+
+namespace uppgift_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett värder");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ett värder");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Skriv in ett värder");
+            int tal3 = int.Parse(Console.ReadLine());
+
+            int summa = Adderar(tal1, tal2, tal3);
+            Console.WriteLine(summa);
+        }
+
+        static int Adderar(int tal1, int tal2, int tal3)
+        {
+          int summa = tal1 + tal2 + tal3;
+            return summa;
+        }
+    }
+}
